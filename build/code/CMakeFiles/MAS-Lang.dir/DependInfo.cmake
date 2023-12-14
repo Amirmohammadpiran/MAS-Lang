4b6f93651bbabeb21babfc35190e78b052d1f78f
Amir <amirdanger80p@gmail.com>
2023-12-14 19:58:17 +0330

    fixed the issue with <= token

---
+++ build/code/CMakeFiles/MAS-Lang.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/CodeGen.cpp" "code/CMakeFiles/MAS-Lang.dir/CodeGen.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/CodeGen.cpp.o.d"
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/Error.cpp" "code/CMakeFiles/MAS-Lang.dir/Error.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/Error.cpp.o.d"
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/Lexer.cpp" "code/CMakeFiles/MAS-Lang.dir/Lexer.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/Lexer.cpp.o.d"
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/Parser.cpp" "code/CMakeFiles/MAS-Lang.dir/Parser.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/Parser.cpp.o.d"
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/Sema.cpp" "code/CMakeFiles/MAS-Lang.dir/Sema.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/Sema.cpp.o.d"
+  "/home/amir/Downloads/CD/Project/MAS-Lang/code/main.cpp" "code/CMakeFiles/MAS-Lang.dir/main.cpp.o" "gcc" "code/CMakeFiles/MAS-Lang.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
