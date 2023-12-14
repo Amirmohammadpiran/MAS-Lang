9f4b139e4693b0be3690ce24232deeb5f3178c4d
Amir Mohammad Piran <95971260+Amirmohammadpiran@users.noreply.github.com>
2023-12-14 20:10:21 +0330

    fixed rtMAS.c

--- rtMAS.c
+++ rtMAS.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void gsm_write(int v)
+void mas_write(int v)
 {
     printf("The result is: %d\n", v);
 }
 
-int gsm_read(char *s)
+int mas_read(char *s)
 {
     char buf[64];
     int val;
